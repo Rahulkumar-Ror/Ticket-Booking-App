96839e763c4bf344908b526deb965a67f03b86e5
Rahul <rahul.k@simformsolutions.com>
2022-05-17 21:43:52 +0530

    Created card token & stripe customer card

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,5 +1,41 @@
+require "stripe"
+
 class StripeService
   def initialize()
     Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+  end
+
+  def find_or_create_customer(customer)
+    if customer.stripe_customer_id.ppresent?
+      stripe_customer = Stripe::Customer.retrieve(customer.
+        stripe_customer_id)
+    else
+      stripe_customer = Stripe::Customer.create({
+        name: customer.full_name,
+        email: customer.email,
+        phone: customer.contact,
+      })
+      customer.update(stripe_customer_id: stripe_customer.id)
+    end
+    stripe_customer
+  end
+
+  def create_card_token(param)
+    Stripe::Token.create({
+      card: {
+        number: params[:card_number].to_s,
+        exp_month: params[:exp_month],
+        exp_year: params[:exp_year],
+        cvv: params[:cvv],
+      },
+    })
+  end
+
+  def create_stripe_customer_card(params, stripe_customer)
+    token = create_card_token(params)
+    Stripe::Customer.create source(
+      stripe_customer.id,
+      { source: token.id }
+    )
   end
 end
