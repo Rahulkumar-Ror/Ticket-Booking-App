311314b27ac1f52431e50d468fb24af336d8c466
Rahul <rahul.k@simformsolutions.com>
2022-06-07 18:24:58 +0530

    Fix to prevent customers to make refund requests for past workshops

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -1,5 +1,5 @@
 class RefundsController < ApplicationController
-  def new 
+	def new 
 	  @refund = Refund.new
 	end
 
@@ -10,13 +10,17 @@ class RefundsController < ApplicationController
 	def create
     booking = Booking.find_by(order_number: params[:refund][:order_number])
 	  if booking.present?
-		@refund = Refund.create(
-			customer_id: booking.customer_id,
-			booking_id: booking.id,
-			state: 'accepted'
-		)
-		redirect_to edit_refund_path(@refund), notice: "Your booking request is valid. Please fill other details to process your refund request."
-   	else
+      if booking.is_refundable?
+		    @refund = Refund.create(
+		    	customer_id: booking.customer_id,
+		    	booking_id: booking.id,
+		    	state: 'accepted'
+		    )
+	     	redirect_to edit_refund_path(@refund), notice: "Your booking request is valid. Please fill other details to process your refund request."
+      else
+				redirect_to new_refund_path, alert: "You are requesting refund for a past workshop. Refunds are only allowed for workshops which are to be taken in the future"
+			end
+		else
 		  redirect_to new_refund_path, alert: "You provided invalid booking ID #{ params[:refund][:order_number] }. We found no booking with this booking id. PLease provide valid booking ID."
 		end
 	end
